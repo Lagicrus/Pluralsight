88ba59d143e73ea95c51ce69877e16196fe31f9a
Sam <Lagicrus@users.noreply.github.com>
2021-12-17 10:19:31 +0000

    part 12 - Building Applications with React and Redux

--- reduxReact/src/components/app.js
+++ reduxReact/src/components/app.js
@@ -5,6 +5,9 @@ import AboutPage from "./about/aboutPage";
 import Header from "./common/header";
 import PageNotFound from "./pageNotFound";
 import CoursesPage from "./courses/coursesPage";
+import ManageCoursePage from "./courses/manageCoursePage";
+import {ToastContainer} from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
     return (
@@ -20,12 +23,19 @@ function App() {
                 <Route path="/courses">
                     <CoursesPage/>
                 </Route>
+                <Route path="/course/:slug">
+                    <ManageCoursePage/>
+                </Route>
+                <Route path="/course">
+                    <ManageCoursePage/>
+                </Route>
                 <Route>
                     <PageNotFound/>
                 </Route>
             </Switch>
+            <ToastContainer autoClose={3000} hideProgressBar/>
         </div>
-    )
+    );
 }
 
 export default App;
